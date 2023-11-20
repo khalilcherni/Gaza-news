5c39762431a433f500f3af16ad90d36a13c76c36
khalilcherni <kcherni411@gmail.com>
2023-11-20 13:06:42 +0000

    Third

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
         window.location.href= 'https://en.wikipedia.org/wiki/History_of_Gaza';
     })
 
-
+$(".car1").click(function(){
+    window.location.href= 'https://www.unicef.org/stories/children-bear-brunt-violence-gaza'; 
+})
+$(".car2").click(function(){
+    window.location.href= 'https://www.unicef.org/stories/children-bear-brunt-violence-gaza'; 
+})
+$(".car3").click(function(){
+    window.location.href= 'https://www.unicef.org/stories/children-bear-brunt-violence-gaza'; 
+})
+$(".car4").click(function(){
+    window.location.href= 'https://www.unicef.org/stories/children-bear-brunt-violence-gaza'; 
+})
    
 })
