c1dbbfcd6c9fd20f6389b030545e2556191d5405
khalilcherni <kcherni411@gmail.com>
2023-11-20 18:27:30 +0000

    seven

--- main.js
+++ main.js
@@ -17,5 +17,17 @@ $(".car3").click(function(){
 $(".car4").click(function(){
     window.location.href= 'https://www.unicef.org/stories/children-bear-brunt-violence-gaza'; 
 })
-   
+
 })
+var heading = document.getElementById("myHeading");
+
+heading.addEventListener("mouseover", function() {
+    heading.innerHTML = "Support Gaza";
+})
+
+heading.addEventListener("mouseout", function() {
+    heading.innerHTML = "Palastine will be free";
+})
+
+
+
